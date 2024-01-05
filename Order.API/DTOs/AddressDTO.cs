8677cfdd8e32ae0180ae2e92fcac6aad2d11dd28
Polat Altunbağ <polataltunbag1@gmail.com>
2024-01-06 02:21:38 +0300

    Order.API oluşturuldu

---
+++ Order.API/DTOs/AddressDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Order.API.DTOs
+{
+    public class AddressDTO
+    {
+
+        public string? Line { get; set; }
+ 
+        public string? Province { get; set; }
+  
+        public string? District { get; set; }
+    }
+}
---
+++ Order.API/Model/Address.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Order.API.Model
+{
+    [Owned]
+    public class Address
+    {
+        [MaxLength(50)]
+        [Required]
+        public string Line { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string Province { get; set; }
+        [MaxLength(50)]
+        [Required]
+        public string District { get; set; }
+    }
+}
